55bac9a7ed0d5313abd4a01073ccd15fd122a67a
Matan157 <matan.uchen@gmail.com>
2015-12-02 21:00:44 +0000

    added sound file and code

--- screamr.py
+++ screamr.py
@@ -20,12 +20,19 @@
 import RPi.GPIO as GPIO
 import time
 
+from Tkinter import *
+root - Tk()
+
+import tkSnack
+tkSnack.initializeSnack(root)
+
+#GPIO setup
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 7 #Pin used for motion detecting
-
-MOTION = False
-
 GPIO.setup(PIR_PIN, GPIO.IN)
+
+#Sound setup
+sound = tkSnack.Sound(load='./scream.wav')
 
 try:
         print "PIR Module Test (CTRL + C to exit)"
@@ -34,6 +41,7 @@ try:
 
         while True: #Main loop for motion detection.
                 if GPIO.input(PIR_PIN):
+                        sound.play()
                         print "Motion Detected!"
                         time.sleep(2)                
                 time.sleep(1)
