cda15517841222726621bb96398af565eae619da
Matan157 <matan.uchen@gmail.com>
2015-12-03 02:07:30 +0000

    sound changes

--- screamr.py
+++ screamr.py
@@ -19,7 +19,9 @@
 
 import RPi.GPIO as GPIO
 import time
-import wx
+import pyaudio
+import wave
+import sys
 
 #GPIO setup
 GPIO.setmode(GPIO.BCM)
@@ -27,7 +29,17 @@ PIR_PIN = 7 #Pin used for motion detecting
 GPIO.setup(PIR_PIN, GPIO.IN)
 
 #Sound setup
-sound = wx.Sound('scream.wav')
+CHUNK = 1024
+
+wf = wave.open('scream.wav', 'rb')
+p = pyaudio.PyAudio()
+
+stream = p.open(format = p.get_format_from_width(wf.getsampwidth(),
+		channels = wf.getnchannels(),
+		rate = wf.getframerate(),
+		output = True)
+
+data = wf.readframes(CHUNK)
 
 try:
         print "PIR Module Test (CTRL + C to exit)"
@@ -36,10 +48,19 @@ try:
 
         while True: #Main loop for motion detection.
                 if GPIO.input(PIR_PIN):
-                        sound.Play(wx.SOUND_SYNC)
+			while len(data) > 0:
+				stream.write(data)
+				data = wf.readframes(CHUNK)
                         print "Motion Detected!"
                         time.sleep(2)                
                 time.sleep(1)
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+
 except KeyboardInterrupt:
+	stream.stop_stream()
+	stream.close()
+	p.teminate()
         print "Quit"
         GPIO.cleanup()
