9a1c958db03ab48b0e2af32faa86dedf35f561d8
Matan157 <matan.uchen@gmail.com>
2015-12-03 03:19:17 +0000

    sound works!

--- screamr.py
+++ screamr.py
@@ -19,28 +19,17 @@
 
 import RPi.GPIO as GPIO
 import time
-import pyaudio
-import wave
-import sys
+
+import pyglet
+
+pyglet.options['audio'] = ('openal', 'silent')
 
 #GPIO setup
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 7 #Pin used for motion detecting
 GPIO.setup(PIR_PIN, GPIO.IN)
 
-#Sound setup
-CHUNK = 1024
-
-wf = wave.open('scream.wav', 'rb')
-p = pyaudio.PyAudio()
-
-stream = p.open(format = p.get_format_from_width(wf.getsampwidth(),
-		channels = wf.getnchannels(),
-		rate = wf.getframerate(),
-		output = True)
-
-data = wf.readframes(CHUNK)
-
+scream = pyglet.media.load('scream2.wav', streaming=False)
 try:
         print "PIR Module Test (CTRL + C to exit)"
         time.sleep(2)
@@ -48,19 +37,12 @@ try:
 
         while True: #Main loop for motion detection.
                 if GPIO.input(PIR_PIN):
-			while len(data) > 0:
-				stream.write(data)
-				data = wf.readframes(CHUNK)
                         print "Motion Detected!"
-                        time.sleep(2)                
+			scream.play()
+			time.sleep(2)
                 time.sleep(1)
-	stream.stop_stream()
-	stream.close()
-	p.terminate()
 
 except KeyboardInterrupt:
-	stream.stop_stream()
-	stream.close()
-	p.teminate()
         print "Quit"
+	pyglet.media.delete()
         GPIO.cleanup()
