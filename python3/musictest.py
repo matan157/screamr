1e93fbf9068cc871d427773ba35a701ed1b9ed92
Matan157 <matan.uchen@gmail.com>
2015-12-05 01:43:02 +0000

    Fixed directory

--- python3/musictest.py
+++
@@ -1,8 +0,0 @@
-from pygame import mixer
-import time
-
-mixer.init()
-mixer.music.load('scream2.wav')
-mixer.music.play(-1)
-time.sleep(10)
-mixer.music.stop()
--- screamr.py
+++ screamr.py
@@ -1,48 +1,62 @@
-# Screamer
-# Created to fuck with mom.
-# Using a PIR motion sensor, the pi will yell whenever someone walks by.
+"""
+    Program by Matan Uchen.
 
-# Cables:
-#	PIR-VCC: Red
-#	PIR-OUT: Brown
-#	PIR-GND: Black
+    This program is was originally written in Python 2.7.
+    After almost no deliberation I decided to move it to Python 3
 
-#Pi GPIO Config:
-#       Red: Pin 2
-#       Black: Pin 6
-#       Brown: Pin 26
+    Using the GPIO pins and a PIR sensor, this program will detect
+    movement. After detecting movement, it will print "Motion Deteced!"
+    to the command line as well as playing the "scream2.wav" file.
 
-#Breadboard
-#       PIR-VCC + Red: Positive Rail
-#       PIR-GRD + Black: Negative Rail
-#       PIR-OUT + Brown: Same Rail (Used 3B + 3C)
+    Here goes nothing...
+
+    PIR Wires:
+        VCC = Red
+        OUT = Brown
+        GND = Black
+
+    GPIO Setup:
+        VCC to 5v
+        OUT to GPIO4
+        GND to GND // duh...
+"""
 
 import RPi.GPIO as GPIO
 import time
 
-import pyglet
+from pygame import mixer
 
-pyglet.options['audio'] = ('openal', 'silent')
+""" GPIO SETUP """
+sensor = 4 #GPIO4
 
-#GPIO setup
 GPIO.setmode(GPIO.BCM)
-PIR_PIN = 7 #Pin used for motion detecting
-GPIO.setup(PIR_PIN, GPIO.IN)
+GPIO.setup(sensor, GPIO.IN, GPIO.PUD_DOWN)
+""" END GPIO """
 
-scream = pyglet.media.load('scream2.wav', streaming=False)
-try:
-        print "PIR Module Test (CTRL + C to exit)"
-        time.sleep(2)
-        print "Ready"
+previous_state = False
+current_state = False
 
-        while True: #Main loop for motion detection.
-                if GPIO.input(PIR_PIN):
-                        print "Motion Detected!"
-			scream.play()
-			time.sleep(2)
-                time.sleep(1)
+# Music Function
+def play():
+    mixer.init()
+    mixer.music.load('siren.mp3') # Change to whatever sound you want.
+    mixer.music.play()
+    time.sleep(5) # 2 for scream || 11 for Bass || 5 for siren
+    mixer.music.stop()
 
-except KeyboardInterrupt:
-        print "Quit"
-	pyglet.media.delete()
-        GPIO.cleanup()
+print("Ready to start. CTRL + C to exit.")
+while True: # Main Loop
+    time.sleep(0.1)
+    previous_state = current_state
+    current_state = GPIO.input(sensor)
+    if current_state != previous_state:
+        if current_state:
+            new_state = "HIGH"
+            print("GPIO pin %s is %s" % (sensor, new_state))
+            play()
+        else:
+            new_state = "LOW"
+            print("GPIO pin %s is %s" % (sensor, new_state))
+
+
+
